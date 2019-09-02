61bee285b1e2829ba040acb595029926bee1d3f7
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-02 23:18:31 +0200

    fixed spec test

--- spec/classes/rules/sec_issue_spec.rb
+++ spec/classes/rules/sec_issue_spec.rb
@@ -2,18 +2,19 @@ require 'spec_helper'
 
 describe 'security_baseline::rules::sec_issue' do
   on_supported_os.each do |os, os_facts|
-    context "on #{os}" dolet(:facts) do
-      os_facts.merge(
-        'issue' => 1234,
-      )
-    end
-    let(:params) do
-      {
-        'enforce' => true,
-        'message' => 'motd configuration',
-        'loglevel' => 'warning',
-      }
-    endlet(:facts) { os_facts }
+    context "on #{os}" do
+      let(:facts) do
+        os_facts.merge(
+          'issue' => 1234,
+        )
+      end
+      let(:params) do
+        {
+          'enforce' => true,
+          'message' => 'motd configuration',
+          'loglevel' => 'warning',
+        }
+      end
 
       it { is_expected.to compile }
     end

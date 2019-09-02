50940f6c849b98eb0cd8a8a18c5ae0e99f817033
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-02 17:17:45 +0200

    fixed undefined and nil return values

--- lib/facter/automounting.rb
+++ lib/facter/automounting.rb
@@ -1,4 +1,14 @@
 Facter.add('automounting') do
   confine :kernel => 'Linux'
-  setcode 'systemctl is-enabled autofs'
+  setcode do
+    ret = ''
+    autofs = Facter::Core::Execution.exec('systemctl is-enabled autofs')
+    if (autofs =~ %r{^Failed}) or (autofs.empty?) then
+      ret = 'disabled'
+    else
+      ret = autofs
+    end
+
+    ret
+  end
 end

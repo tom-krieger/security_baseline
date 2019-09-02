bbd5a29e4624c0849c1489f1ece21e2045e44737
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-02 18:37:42 +0200

    removed do block

--- lib/facter/aide_version.rb
+++ lib/facter/aide_version.rb
@@ -4,7 +4,5 @@
 # Ensures that aide is installed and create a fact with the version
 Facter.add('aide_version') do
   confine :odfamily => 'RedHat'
-  setcode do
-    Facter::Core::Execution.exec("rpm -q --queryformat '%{version}' aide")
-  end
+  setcode "rpm -q --queryformat '%{version}' aide"
 end

5a70fe1e38af46a59c8b181e89550f574d3d5dd8
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-18 14:59:07 +0100

    fixed typo

--- lib/facter/security_baseline.rb
+++ lib/facter/security_baseline.rb
@@ -283,7 +283,7 @@ Facter.add(:security_baseline) do
 
     security_baseline[:sticky_ww] = Facter::Core::Execution.exec("df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null")
 
-    security_baseline[:security_patches] = Facter::Core.Execution.exec('yum check-update --security -q | grep -v ^$')
+    security_baseline[:security_patches] = Facter::Core::Execution.exec('yum check-update --security -q | grep -v ^$')
 
     security_baseline[:gnome_gdm] = Facter::Core::Execution.exec('rpm -qa | grep gnome') != ''
 

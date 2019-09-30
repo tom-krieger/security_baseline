ec1d3a800a4bc92274bebb37302a286034541a50
Thomas Krieger <thomas.krieger@puppet.com>
2019-09-30 17:35:13 +0200

    fixed typo

--- manifests/rules/sec_crond.pp
+++ manifests/rules/sec_crond.pp
@@ -31,7 +31,7 @@ class security_baseline::rules::sec_crond (
 ) {
   if($enforce) {
     if($facts['osfamily'] == 'RedHat') {
-      $serv = 'crond'
+      $srv = 'crond'
     } else {
       $srv = 'cron'
     }

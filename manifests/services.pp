65b09dd3321518acd297b200bdc26475c300508c
Thomas Krieger <thomas.krieger@puppet.com>
2019-10-02 15:07:48 +0200

    use reload for sshd

--- manifests/services.pp
+++ manifests/services.pp
@@ -7,7 +7,7 @@
 #   include security_baseline::services
 class security_baseline::services {
   exec { 'reload-sshd':
-    command     => 'pkill -HUP sshd',
+    command     => 'systemctl reloadf sshd',
     path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
     refreshonly => true,
   }

0209f4f6453f43c397e0d089c506c8c4a0df0def
Thomas Krieger <thomas.krieger@puppet.com>
2019-10-09 14:10:15 +0200

    added debug output

--- manifests/sec_check.pp
+++ manifests/sec_check.pp
@@ -87,6 +87,7 @@ define security_baseline::sec_check (
         if($current_value) {
 
           if(is_array($current_value) and is_array($fact_value)) {
+            info('arrays')
             unless(member($current_value, $fact_value)) {
               echo { "Fact ${fact_name} should have value '${fact_value}' but has current value '${current_value}'":
                 loglevel => $loglevel,

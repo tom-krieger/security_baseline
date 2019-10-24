cb2eccbc4f8205dc58602890cd450c1d2206d4a3
Thomas Krieger <thomas.krieger@puppet.com>
2019-10-24 16:14:18 +0100

    formatting

--- lib/facter/helpers/check_value_integer.rb
+++ lib/facter/helpers/check_value_integer.rb
@@ -3,10 +3,10 @@
 # check a value and return a value depending on conditions
 
 def check_value_integer(val, default = 0)
-    if val.nil? || val.empty?
-      default.to_i
-    else
-      val.to_i
-    end
+  if val.nil? || val.empty?
+    default.to_i
+  else
+    val.to_i
   end
+end
   

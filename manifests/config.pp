04b9c0fdddb67f21917aab5cbb4ff6aad1e8c04f
Thomas Krieger <tom@tom-krieger.de>
2019-12-02 16:40:34 +0100

    moved puppet agent config to config class

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,7 @@
-# @summary A short summary of the purpose of this class
+# @summary 
+#    Configuration stuff
 #
-# A description of what this class does
+# Run configuration stuff
 #
 # @example
 #   include security_baseline::config
@@ -90,5 +91,27 @@ class security_baseline::config {
     owner  => 'root',
     group  => 'root',
     mode   => '0700',
+  }
+
+  if(('security_baseline' in $facts) and ('puppet_agent_postrun' in $facts['security_baseline'])) {
+    if ($facts['security_baseline']['puppet_agent_postrun'] != 'postrun_command = /usr/local/bin/puppet facts upload') {
+      exec { 'set puppet agent postrun agent':
+        command => 'puppet config --section agent set postrun_command "/usr/local/bin/puppet facts upload"',
+        path    => ['/bin', '/usr/bin', '/usr/local/bin'],
+      }
+      exec { 'set puppet agent postrun main':
+        command => 'puppet config --section main set postrun_command "/usr/local/bin/puppet facts upload"',
+        path    => ['/bin', '/usr/bin', '/usr/local/bin'],
+      }
+    }
+  } else {
+    exec { 'set puppet agent postrun agent':
+      command => 'puppet config --section agent set postrun_command "/usr/local/bin/puppet facts upload"',
+      path    => ['/bin', '/usr/bin', '/usr/local/bin'],
+    }
+    exec { 'set puppet agent postrun main':
+      command => 'puppet config --section main set postrun_command "/usr/local/bin/puppet facts upload"',
+      path    => ['/bin', '/usr/bin', '/usr/local/bin'],
+    }
   }
 }

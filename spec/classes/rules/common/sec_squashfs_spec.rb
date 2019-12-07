f39e2de04a1de478c6dc87144aa84e6933c8a1c0
Thomas Krieger <tom@tom-krieger.de>
2019-12-07 23:58:14 +0100

    fixed tests

--- spec/classes/rules/common/sec_squashfs_spec.rb
+++ spec/classes/rules/common/sec_squashfs_spec.rb
@@ -1,21 +1,5 @@
 require 'spec_helper'
 
-describe 'security_baseline::rules::common::sec_squashfs' do
-  on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:facts) { os_facts }
-      let(:params) do
-        {
-          'enforce' => true,
-          'message' => 'squashfs',
-          'loglevel' => 'warning',
-        }
-      end
-
-      it { is_expected.to compile }
-    end
-  end
-end
 enforce_options = [true, false]
 
 describe 'security_baseline::rules::common::sec_squashfs' do

53a4932ba6207707e32ff423ccb2216a4b28e4d5
Thomas Krieger <tom@tom-krieger.de>
2019-12-10 16:13:24 +0100

    formatting

--- manifests/logging.pp
+++ manifests/logging.pp
@@ -43,17 +43,17 @@ define security_baseline::logging (
 ) {
   if($reporting_type == 'fact') {
       concat::fragment { $rulenr:
-      content => epp('security_baseline/logentry.epp', {
-        'rulenr'    => $rulenr,
-        'rule'      => $rule,
-        'desc'      => $desc,
-        'msg'       => $msg,
-        'loglevel'  => $log_level,
-        'rulestate' => $rulestate,
-        'level'     => $level,
-        'scored'    => $scored,
-      }),
-      target  => $::security_baseline::logfile,
+        content => epp('security_baseline/logentry.epp', {
+          'rulenr'    => $rulenr,
+          'rule'      => $rule,
+          'desc'      => $desc,
+          'msg'       => $msg,
+          'loglevel'  => $log_level,
+          'rulestate' => $rulestate,
+          'level'     => $level,
+          'scored'    => $scored,
+        }),
+        target  => $::security_baseline::logfile,
     }
   }
 }

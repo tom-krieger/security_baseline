8a141d304b86febeed1424d9caa17398883f4bc2
Thomas Krieger <tom@tom-krieger.de>
2019-12-10 22:31:46 +0100

    check kernel module with lsmod and modprobe

--- lib/facter/security_baseline/common/check_kernel_module.rb
+++ lib/facter/security_baseline/common/check_kernel_module.rb
@@ -1,8 +1,9 @@
 # check if a kernelmodule is installed
 
 def check_kernel_module(mod)
+  instmod = Facter::Core::Execution.exec("modprobe -n -v #{mod}")
   installed = Facter::Core::Execution.exec("lsmod | grep #{mod}")
-  if installed.empty?
+  if installed.empty? && (instmod.match(%r{install\s*/bin/true}) || instmod.match(%r{not found})
     false
   else
     true

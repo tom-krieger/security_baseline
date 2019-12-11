77fe30f2bab98957ec9900d912c71725080e336d
Thomas Krieger <tom@tom-krieger.de>
2019-12-11 15:25:30 +0100

    added debug output

--- manifests/sec_check.pp
+++ manifests/sec_check.pp
@@ -87,14 +87,19 @@ define security_baseline::sec_check (
       }
 
       $fact_name = $check['fact_name']
-      if($fact_name != '') {
+      unless (empty($fact_name)) {
         $fact_value = $check['fact_value']
         $data_hash  = $facts[$check['fact_hash']]
 
-        if(! $data_hash.empty()) {
+        if(!$data_hash.empty()) {
           $current_value = dig($data_hash, *$fact_name)
         } else {
           $current_value = $facts[$fact_name]
+        }
+
+        echo { "current value #{current_value}":
+          loglevel => 'info',
+          withpath => false,
         }
 
         if($current_value) {

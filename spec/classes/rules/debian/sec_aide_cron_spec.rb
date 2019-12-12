56a4f4336840737fd9fcb6cc1483cf0ef3374e85
Thomas Krieger <tom@tom-krieger.de>
2019-12-12 11:41:36 +0100

    added debian versions

---
+++ spec/classes/rules/debian/sec_aide_cron_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+enforce_options = [true, false]
+
+describe 'security_baseline::rules::debian::sec_aide_cron' do
+  enforce_options.each do |enforce|
+    on_supported_os.each do |os, os_facts|
+      context "on #{os} with enforce = #{enforce}" do
+        let(:facts) do
+          os_facts.merge(
+            'security_baseline' => {
+              'aide' => {
+                'version' =>  '6.1.2',
+                'status' => 'installed',
+              },
+            },
+          )
+        end
+        let(:params) do
+          {
+            'enforce' => enforce,
+            'message' => 'aide cron',
+            'log_level' => 'warning',
+          }
+        end
+
+        it { is_expected.to compile }
+        it do
+          if enforce
+            is_expected.to contain_file('/etc/cron.d/aide.cron')
+              .with(
+                'ensure' => 'present',
+                'owner'  => 'root',
+                'group'  => 'root',
+                'mode'   => '0644',
+              )
+            is_expected.not_to contain_echo('aide-cron')
+          else
+            is_expected.to contain_echo('aide-cron')
+              .with(
+                'message'  => 'aide cron',
+                'loglevel' => 'warning',
+                'withpath' => false,
+              )
+          end
+        end
+      end
+    end
+  end
+end

35478fa232aa8ebc0d6083fe553410b5b5003158
Thomas Krieger <tom@tom-krieger.de>
2019-12-12 20:54:20 +0100

    added params

--- manifests/rules/debian/sec_service_openbsd_inetd.pp
+++ manifests/rules/debian/sec_service_openbsd_inetd.pp
@@ -4,5 +4,9 @@
 #
 # @example
 #   include security_baseline::rules::debian::sec_service_openbsd_inetd
-class security_baseline::rules::debian::sec_service_openbsd_inetd {
+class security_baseline::rules::debian::sec_service_openbsd_inetd (
+  Boolean $enforce  = true,
+  String $message   = '',
+  String $log_level = ''
+) {
 }

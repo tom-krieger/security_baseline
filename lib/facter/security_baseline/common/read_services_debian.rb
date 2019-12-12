96d7300090de4a260a882b5d6ce5175b8e3a8a0d
Thomas Krieger <tom@tom-krieger.de>
2019-12-12 20:39:26 +0100

    test

--- lib/facter/security_baseline/common/read_services_debian.rb
+++ lib/facter/security_baseline/common/read_services_debian.rb
@@ -2,25 +2,30 @@ def read_service_data(services, file_name)
   srv_name = ''
   srv_status = ''
   srv_id = ''
+  srv = false
   File.readlines(file_name).each do |line|
     next if line.match(%r{^$}) || line.match(%r{^#})
     if line =~ %r{^service}
       m = line.match(%r{service\s*(?<name>\w)})
       srv_name = m[:name]
-    elsif line =~ %r{disable\s*=}
+      srv = true
+    elsif line =~ %r{disable\s*=} && srv
       m = line.match(%r{disable\s*=\s*(?<status>\w)})
       srv_status = m[:status]
-    elsif line =~ %r{id\s*=}
+    elsif line =~ %r{id\s*=} && srv
       m = line.match(%r{id=\s*=\s*(?<srvid>\w)})
       srv_id = m[:srvid]
-    elsif line =~ %r{^\}}
+    elsif line =~ %r{^\}} && srv
       if srv_id == ''
         srv_id = srv_name
       end
-      services["srv_#{srv_id}"] = !srv_status.casecmp('yes').zero?
+      if srv
+        services["srv_#{srv_id}"] = !srv_status.casecmp('yes').zero?
+      end
       srv_name = ''
       srv_status = ''
       srv_id = ''
+      srv = false
     end
   end
 

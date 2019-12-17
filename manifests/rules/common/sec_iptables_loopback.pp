81eb4bc59b590a36524463a2889f93d8a0824ef1
Thomas Krieger <tom@tom-krieger.de>
2019-12-17 12:01:14 +0100

    test

--- manifests/rules/common/sec_iptables_loopback.pp
+++ manifests/rules/common/sec_iptables_loopback.pp
@@ -34,6 +34,18 @@ class security_baseline::rules::common::sec_iptables_loopback (
   String $message   = '',
   String $log_level = '',
 ) {
+  $rule1 = $facts['security_baseline']['iptables']['policy'].filter |$rule, $data| {
+    $data['chain'] == 'INPUT' and $data['proto'] == 'all' and $data['in'] == 'lo' and
+    $data['src'] == '0.0.0.0/0' and $data['dst'] == '0.0.0.0/0' and $data['target'] == 'ACCEPT'
+  }
+  $rule2 = $facts['security_baseline']['iptables']['policy'].filter |$rule, $data| {
+    $data['chain'] == 'OUTPUT' and $data['proto'] == 'all' and $data['out'] == 'lo' and
+    $data['src'] == '0.0.0.0/0' and $data['dst'] == '0.0.0.0/0' and $data['target'] == 'ACCEPT'
+  }
+  $rule3 = $facts['security_baseline']['iptables']['policy'].filter |$rule, $data| {
+    $data['chain'] == 'INPUT' and $data['proto'] == 'all' and
+    $data['src'] == '127.0.0.0/8' and $data['dst'] == '0.0.0.0/0' and $data['target'] == 'DROPI'
+  }
   if ($enforce) {
     firewall { '001 accept all incoming traffic to local interface':
       chain   => 'INPUT',
@@ -56,12 +68,18 @@ class security_baseline::rules::common::sec_iptables_loopback (
       require => Firewall['002 accept all outgoing traffic to local interface'],
     }
   } else {
-    $rule1 = $facts['security_baseline']['iptables']['policy'].filter |$rule, $data| {
-      $data['chain'] == 'INPUT' and $data['proto'] == 'all' and $data['in'] == 'lo' and
-      $data['src'] == '0.0.0.0/0' and $data['dst'] == '0.0.0.0/0' and $data['target'] == 'ACCEPT'
-    }
-    echo { "iptables-ploopback ${rule1}":
+    echo { "iptables-loopback ${rule1}":
       message  => "${message} ${rule1}",
+      loglevel => $log_level,
+      withpath => false,
+    }
+    echo { "iptables-loopback ${rule2}":
+      message  => "${message} ${rule2}",
+      loglevel => $log_level,
+      withpath => false,
+    }
+    echo { "iptables-loopback ${rule3}":
+      message  => "${message} ${rule3}",
       loglevel => $log_level,
       withpath => false,
     }

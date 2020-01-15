2a08d9f157175fc835d98d75f1f6ec0bc3869368
Thomas Krieger <tom@tom-krieger.de>
2020-01-15 11:52:34 +0100

    changed ubunto intactive boot

--- manifests/rules/debian/sec_interctive_boot.pp
+++ manifests/rules/debian/sec_interctive_boot.pp
@@ -33,7 +33,13 @@ class security_baseline::rules::debian::sec_interctive_boot (
   String $log_level = ''
 ) {
   if ($enforce) {
-    if($facts['security_baseline']['interactive_boot'] != 'n/a') {
+    if($facts['security_baseline']['interactive_boot'] != 'no') {
+      file { '/etc/sysconfig/boot':
+        ensure => file,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+      }
       file_line { 'interactive-boot':
         ensure             => 'present',
         path               => '/etc/sysconfig/boot',

062175bf154ef52aba4e8cb19b7339ed8d09076c
Thomas Krieger <tom@tom-krieger.de>
2020-01-18 13:21:55 +0100

    added selinux packages

--- manifests/rules/debian/sec_selinux_bootloader.pp
+++ manifests/rules/debian/sec_selinux_bootloader.pp
@@ -40,6 +40,16 @@ class security_baseline::rules::debian::sec_selinux_bootloader (
           loglevel => 'warning',
           withpath => false,
         }
+        if(!defined(Package['selinux-basics'])) {
+          package {'selinux-basics':
+            ensure => installed,
+          }
+        }
+        if(!defined(Package['selinux-policy-default'])){
+          package {'selinux-policy-default':
+            ensure => installed,
+          }
+        }
         exec { 'activate selinux':
           command => 'selinux-activate',
           path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],

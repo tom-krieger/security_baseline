346771d429d03b1bdf509906f539535433669ecb
Thomas Krieger <tom@tom-krieger.de>
2020-01-24 21:34:57 +0100

    exception handling

--- lib/puppet/functions/security_baseline/init.rb
+++ lib/puppet/functions/security_baseline/init.rb
@@ -7,10 +7,14 @@ Puppet::Functions.create_function(:'security_baseline::init') do
     call_function('debug', 'init called')
     call_function('info', 'init called')
     File.delete(filename) if File.exist?(filename)
-    File.open(filename, 'w') do |file|
-      file.puts("ok:\n")
-      file.puts("fail:\n")
-      file.puts("unknown:\n")
+    begin
+      File.open(filename, 'w') do |file|
+        file.puts("ok:\n")
+        file.puts("fail:\n")
+        file.puts("unknown:\n")
+      end
+    rescue
+      raise Puppet::ParseError, ("security_baseline::init failed to write file #{filename}")
     end
 
     nil

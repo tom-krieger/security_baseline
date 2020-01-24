1164cca1f445f7d86ef5184049d3455478a2b1f3
Thomas Krieger <tom@tom-krieger.de>
2020-01-24 19:56:33 +0100

    fixed typo

--- lib/puppet/parser/functions/security_baseline/init.rb
+++ lib/puppet/parser/functions/security_baseline/init.rb
@@ -3,10 +3,10 @@ module Puppet::Parser::Functions
     newfunction(:'security_baseline::init', :type => :rvalue, :doc => <<-DOC) do |args|
       @summary
           Initialize summary file
-          
+
       DOC
 
-      raise Puppet::ParseError, "security_baseline::init(): Wrong number of arguments (#{args.length}; must be = 1)" unless args.length = 1
+      raise Puppet::ParseError, "security_baseline::init(): Wrong number of arguments (#{args.length}; must be = 1)" unless args.length == 1
 
       filename = args[0]
       File.delete(filename) if File.exist?(filename)

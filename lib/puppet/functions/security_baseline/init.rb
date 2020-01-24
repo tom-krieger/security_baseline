a42eb50dcf116c92bd0b5ccf52a385e730f20995
Thomas Krieger <tom@tom-krieger.de>
2020-01-24 21:17:30 +0100

    added info

--- lib/puppet/functions/security_baseline/init.rb
+++ lib/puppet/functions/security_baseline/init.rb
@@ -5,6 +5,7 @@ Puppet::Functions.create_function(:'security_baseline::init') do
 
   def init(filename)
     call_function('debug', 'init called')
+    call_function('info', 'init called')
     File.delete(filename) if File.exist?(filename)
     File.open(filename, 'w') do |file|
       file.puts("ok:\n")

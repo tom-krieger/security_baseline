92271d41f300236679ffe9702cc758ca805c267b
Thomas Krieger <tom@tom-krieger.de>
2020-01-24 16:41:34 +0100

    add initial content

--- lib/puppet/functions/security_baseline/init.rb
+++ lib/puppet/functions/security_baseline/init.rb
@@ -4,5 +4,10 @@ Puppet::Functions.create_function(:'security_baseline::init') do
 
   def init
     File.delete('/tmp/security_baseline_summary.txt') if File.exist?('/tmp/security_baseline_summary.txt')
+    File.open('/tmp/security_baseline_summary.txt') { |file|
+      file.write("ok:\n")
+      file.write("fail:\n")
+      file.write("unknown:\n")
+    }
   end
 end

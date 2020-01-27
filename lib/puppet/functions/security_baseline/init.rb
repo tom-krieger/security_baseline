d591358501189d8bfe835a972297b29a8e8efe39
Thomas Krieger <tom@tom-krieger.de>
2020-01-27 13:26:09 +0100

    use guard clause

--- lib/puppet/functions/security_baseline/init.rb
+++ lib/puppet/functions/security_baseline/init.rb
@@ -4,9 +4,9 @@ Puppet::Functions.create_function(:'security_baseline::init') do
   end
 
   def init(filename = '/tmp/security_baseline_summary.txt')
-    if File.exist?(filename)
-      call_function('info', "init cleanup #{filename}")
-      File.delete(filename)
-    end
+    return unless File.exist?(filename)
+
+    call_function('info', "init cleanup #{filename}")
+    File.delete(filename)
   end
 end

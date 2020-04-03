0c45b5ea8d2bfdfc4dbf60642001e730ec40b946
Thomas Krieger <tom@tom-krieger.de>
2020-04-03 08:50:06 +0200

    current test setup

--- scripts/run_acceptance_tests.sh
+++ scripts/run_acceptance_tests.sh
@@ -6,16 +6,16 @@ pdk bundle install --quiet
 # fire up two docker containers
 # bundle exec rake 'litmus:provision[docker, centos:7]'
 # bundle exec rake 'litmus:provision[docker, ubuntu:18.04]'
-pdk bundle exec rake 'litmus:provision[vagrant, centos/8]'
-# pdk bundle exec rake 'litmus:provision_list[dev_vagrant_all]'
+# pdk bundle exec rake 'litmus:provision[vagrant, centos/7]'
+pdk bundle exec rake 'litmus:provision_list[dev_vagrant_all]'
 
 # install Puppet agent
 pdk bundle exec rake litmus:install_agent
 
 # create symlink for puppet in vagrant environments
 # workarround for secure_path settings in sudoers file
-pdk bundle exec bolt command run '[ -f /usr/local/bin/puppet ] || ln -s /opt/puppetlabs/puppet/bin/puppet /usr/local/bin/puppet' -i inventory.yaml --targets ssh_nodes
-pdk bundle exec bolt command run '[ -f /usr/bin/puppet ] || ln -s /opt/puppetlabs/puppet/bin/puppet /usr/bin/puppet' -i inventory.yaml --targets ssh_nodes
+# pdk bundle exec bolt command run '[ -f /usr/local/bin/puppet ] || ln -s /opt/puppetlabs/puppet/bin/puppet /usr/local/bin/puppet' -i inventory.yaml --targets ssh_nodes
+# pdk bundle exec bolt command run '[ -f /usr/bin/puppet ] || ln -s /opt/puppetlabs/puppet/bin/puppet /usr/bin/puppet' -i inventory.yaml --targets ssh_nodes
 
 # install Puppet module to test
 pdk bundle exec rake litmus:install_module
@@ -31,15 +31,15 @@ pdk bundle exec rake litmus:install_module
 #pdk bundle exec bolt file upload spec/fixtures/hiera/hiera.yaml /etc/puppetlabs/code/environments/production/modules/security_baseline/hiera.yaml -i inventory.yaml --targets ssh_nodes
 
 # run tests in parallel with less output
-# pdk bundle exec rake litmus:acceptance:parallel
+pdk bundle exec rake litmus:acceptance:parallel
 
 # run tests with more output
 # TARGET_HOST=localhost:2222 pdk bundle exec rspec ./spec/acceptance --format d
 # TARGET_HOST=localhost:2223 pdk bundle exec rspec ./spec/acceptance --format d
 
-TARGET_HOST=127.0.0.1:2222 pdk bundle exec rspec ./spec/acceptance --format d
+# TARGET_HOST=127.0.0.1:2222 pdk bundle exec rspec ./spec/acceptance --format d
 
 # tear down the test environment
-# pdk bundle exec rake litmus:tear_down
+pdk bundle exec rake litmus:tear_down
 
 exit 0

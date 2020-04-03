504157524db704a0ace9395f9ecca02bd65457fb
Thomas Krieger <tom@tom-krieger.de>
2020-04-03 08:51:30 +0200

    removed unnecessary commented code

--- scripts/run_acceptance_tests.sh
+++ scripts/run_acceptance_tests.sh
@@ -12,23 +12,8 @@ pdk bundle exec rake 'litmus:provision_list[dev_vagrant_all]'
 # install Puppet agent
 pdk bundle exec rake litmus:install_agent
 
-# create symlink for puppet in vagrant environments
-# workarround for secure_path settings in sudoers file
-# pdk bundle exec bolt command run '[ -f /usr/local/bin/puppet ] || ln -s /opt/puppetlabs/puppet/bin/puppet /usr/local/bin/puppet' -i inventory.yaml --targets ssh_nodes
-# pdk bundle exec bolt command run '[ -f /usr/bin/puppet ] || ln -s /opt/puppetlabs/puppet/bin/puppet /usr/bin/puppet' -i inventory.yaml --targets ssh_nodes
-
 # install Puppet module to test
 pdk bundle exec rake litmus:install_module
-
-#pdk bundle exec bolt command run 'rm -f /etc/puppetlabs/code/environments/production/modules/security_baseline/data/*' -i inventory.yaml --targets ssh_nodes
-
-#for f in `ls spec/fixtures/hiera/data/`; do
-#   fn=`basename ${f}`
-#   echo "uploading spec/fixtures/hiera/data/${f} to /etc/puppetlabs/code/environments/production/modules/security_baseline/data/${fn}"
-#   bolt file upload spec/fixtures/hiera/data/${f} /etc/puppetlabs/code/environments/production/modules/security_baseline/data/${fn} -i inventory.yaml --targets ssh_nodes
-#done
-
-#pdk bundle exec bolt file upload spec/fixtures/hiera/hiera.yaml /etc/puppetlabs/code/environments/production/modules/security_baseline/hiera.yaml -i inventory.yaml --targets ssh_nodes
 
 # run tests in parallel with less output
 pdk bundle exec rake litmus:acceptance:parallel

6799bef7d02d5cf0ce17d1fc1ed4e2fd498fda2f
Thomas Krieger <tom@tom-krieger.de>
2020-04-09 19:27:59 +0200

    fixed further sed commands

--- files/update_pam_pw_hash_sha512_config.sh
+++ files/update_pam_pw_hash_sha512_config.sh
@@ -2,7 +2,7 @@
 
 PTF=$1
 
-[[ -z $(grep -E '^\s*password\s+sufficient\s+pam_unix.so\s+.*sha512\s*.*$' $PTF) ]] && sed - ri 's/^\s*(password\s+sufficient\s+pam_unix.so\s+)(.*)$/\1\2 sha512/' $PTF 
+[[ -z $(grep -E '^\s*password\s+sufficient\s+pam_unix.so\s+.*sha512\s*.*$' $PTF) ]] && sed -ri 's/^\s*(password\s+sufficient\s+pam_unix.so\s+)(.*)$/\1\2 sha512/' $PTF 
   
 
 authselect apply-changes

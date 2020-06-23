02cf9fc063d064f1c3c17e351ecf1ddb52a877ed
Thomas Krieger <tom@tom-krieger.de>
2020-06-23 10:10:06 +0200

    acceptance tests

--- spec/shared/os/redhat/6.rb
+++ spec/shared/os/redhat/6.rb
@@ -1,12 +1,11 @@
 shared_examples 'os::redhat::6' do
-    puts 'RedHat 6 tests' if ENV['DEBUG'] == '1'
-    include_examples 'common::cramfs'
-    include_examples 'common::freexvfs'
-    include_examples 'common::jffs2'
-    include_examples 'common::hfs'
-    include_examples 'common::hfsplus'
-    include_examples 'common::squashfs'
-    include_examples 'common::udf'
-    include_examples 'common::vfat'
-  end
-  
+  puts 'RedHat 6 tests' if ENV['DEBUG'] == '1'
+  include_examples 'common::cramfs'
+  include_examples 'common::freexvfs'
+  include_examples 'common::jffs2'
+  include_examples 'common::hfs'
+  include_examples 'common::hfsplus'
+  include_examples 'common::squashfs'
+  include_examples 'common::udf'
+  include_examples 'common::vfat'
+end
